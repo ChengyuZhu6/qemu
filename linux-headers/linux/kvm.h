86bf9a96613e2a08af6ff6b84380f1608c7423d5
Yao Yuan <yuan.yao@intel.com>
2022-04-14 14:35:59 +0800

    linux-headers: ioctl KVM_MEMORY_ENCRYPT_{READ,WRITE}_MEMORY
    Pull only KVM_MEMORY_ENCRYPT_{READ,WRITE}_MEMORY ioctl
    definition to make this series workable.
    
    These ioctl definition should be updated if the kernel
    part is changed to different values.
    
    Signed-off-by: Yao Yuan <yuan.yao@intel.com>

--- linux-headers/linux/kvm.h
+++ linux-headers/linux/kvm.h
@@ -1238,6 +1238,8 @@ struct kvm_ppc_resize_hpt {
 #define KVM_CAP_SYSTEM_EVENT_DATA 215
 #define KVM_CAP_X86_TRIPLE_FAULT_EVENT 218
 #define KVM_CAP_X86_NOTIFY_VMEXIT 219
+#define KVM_CAP_ENCRYPT_MEMORY_DEBUG 300
+
 #define KVM_CAP_VM_TYPES 1000
 
 #ifdef KVM_CAP_IRQ_ROUTING
@@ -1866,6 +1868,10 @@ struct kvm_xen_vcpu_attr {
 #define KVM_XEN_VCPU_ATTR_TYPE_VCPU_ID		0x6
 #define KVM_XEN_VCPU_ATTR_TYPE_TIMER		0x7
 #define KVM_XEN_VCPU_ATTR_TYPE_UPCALL_VECTOR	0x8
+
+/* Read/write encrypted guest memory, for guest debugging support in QEMU*/
+#define KVM_MEMORY_ENCRYPT_READ_MEMORY   _IOWR(KVMIO, 0xcc, struct kvm_rw_memory)
+#define KVM_MEMORY_ENCRYPT_WRITE_MEMORY  _IOWR(KVMIO, 0xcd, struct kvm_rw_memory)
 
 /* Secure Encrypted Virtualization command */
 enum sev_cmd_id {
